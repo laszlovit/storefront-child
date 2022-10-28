dac124fb7da8886ac3bea10f4c2500c904978981
laszlovit <laszlo.vitkai@gmail.com>
2022-10-28 13:20:44 +0200

    first commit

---
+++ page-home.php
@@ -0,0 +1,113 @@
+<?php 
+get_header(); 
+
+?>
+
+<body>
+   <!-- Banner -->
+   <div class="bg-indigo-600 mb-6">
+      <div class="mx-auto max-w-7xl py-3 px-3 sm:px-6 lg:px-8">
+         <div class="flex flex-wrap items-center justify-center">
+            <div class="flex items-center">
+               <span class="flex rounded-lg bg-indigo-800 p-2">
+                  <!-- Heroicon name: outline/megaphone -->
+                  <svg class="h-6 w-6 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+                     <path stroke-linecap="round" stroke-linejoin="round" d="M10.34 15.84c-.688-.06-1.386-.09-2.09-.09H7.5a4.5 4.5 0 110-9h.75c.704 0 1.402-.03 2.09-.09m0 9.18c.253.962.584 1.892.985 2.783.247.55.06 1.21-.463 1.511l-.657.38c-.551.318-1.26.117-1.527-.461a20.845 20.845 0 01-1.44-4.282m3.102.069a18.03 18.03 0 01-.59-4.59c0-1.586.205-3.124.59-4.59m0 9.18a23.848 23.848 0 018.835 2.535M10.34 6.66a23.847 23.847 0 008.835-2.535m0 0A23.74 23.74 0 0018.795 3m.38 1.125a23.91 23.91 0 011.014 5.395m-1.014 8.855c-.118.38-.245.754-.38 1.125m.38-1.125a23.91 23.91 0 001.014-5.395m0-3.46c.495.413.811 1.035.811 1.73 0 .695-.316 1.317-.811 1.73m0-3.46a24.347 24.347 0 010 3.46" />
+                  </svg>
+               </span>
+               <p class="ml-3 truncate font-medium text-white">
+                  <span class="md:hidden">Free shipping over 400 kr. </span>
+                  <span class="hidden md:inline">Free shipping over 400 kr.</span>
+               </p>
+            </div>
+         </div>
+      </div>
+   </div>
+   <!--Hero section--> 
+   <section class="text-gray-600 body-font">
+      <div class="container mx-auto flex px-5 md:flex-row flex-col items-center">
+         <div class="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+            <h1 class="title-font sm:text-4xl text-3xl mb-4 font-bold text-indigo-700">Simple, high quality
+               <br class="hidden lg:inline-block">unbranded clothing.
+            </h1>
+            <p class="mb-8 leading-relaxed">Copper mug try-hard pitchfork pour-over freegan heirloom neutra air plant cold-pressed tacos poke beard tote bag. Heirloom echo park mlkshk tote bag selvage hot chicken authentic tumeric truffaut hexagon try-hard chambray.</p>
+            <div class="flex justify-center">
+               <a target="_self" href="http://webshop.local/shop/"> 
+               <button class="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Find out more</button>
+               </a> 
+            </div>
+         </div>
+         <div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+            <img class="object-cover object-center rounded shadow-lg" alt="hero" src="http://webshop.local/wp-content/uploads/2022/10/storefront_hero.jpg">
+         </div>
+      </div>
+   </section>
+   <!-- Product categories section -->
+   <section class="text-gray-600 body-font">
+      <div class="container px-5 py-24 mx-auto">
+         <div class="flex flex-col">
+            <div class="h-1 bg-gray-200 rounded overflow-hidden">
+               <div class="w-24 h-full bg-indigo-500"></div>
+            </div>
+            <div class="flex flex-wrap sm:flex-row flex-col py-6 mb-12">
+               <h2 class="sm:w-2/5 text-gray-900 font-medium title-font text-2xl mb-2 sm:mb-0">All the essentials you need</h2>
+               <p class="sm:w-3/5 leading-relaxed text-base sm:pl-10 pl-0">Street art subway tile salvia four dollar toast bitters selfies quinoa yuccie synth meditation iPhone intelligentsia prism tofu. Viral gochujang bitters dreamcatcher.</p>
+            </div>
+         </div>
+         <div class="flex flex-wrap sm:-m-4 -mx-4 -mb-10 -mt-4 divide-x-2">
+            <div class="p-4 md:w-1/3 sm:mb-0 mb-6">
+               <div class="rounded-lg h-64 overflow-hidden shadow-lg ">
+                  <img alt="content" class="object-cover object-center h-full w-full" src="http://webshop.local/wp-content/uploads/2022/10/hoodie_blue.jpg">
+               </div>
+               <div class="flex flex-col text-center items-center">
+                  <h3 class="text-xl font-semibold title-font text-gray-900 mt-5">Hoodies</h3>
+                  <p class="text-base leading-relaxed mt-2">Swag shoivdigoitch literally meditation subway tile tumblr cold-pressed. Gastropub street art beard dreamcatcher neutra, ethical XOXO lumbersexual.</p>
+                  <a target="_self" href="http://webshop.local/shop/" class="text-indigo-500 inline-flex items-center mt-3">
+                     Learn More
+                     <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
+                        <path d="M5 12h14M12 5l7 7-7 7"></path>
+                     </svg>
+                  </a>
+               </div>
+            </div>
+            <div class="p-4 md:w-1/3 sm:mb-0 mb-6">
+               <div class="rounded-lg h-64 overflow-hidden shadow-lg">
+                  <img alt="content" class="object-cover object-center h-full w-full" src="http://webshop.local/wp-content/uploads/2022/10/white_t-shirt.jpg">
+               </div>
+               <div class="flex flex-col text-center items-center">
+                  <h3 class="text-xl font-semibold title-font text-gray-900 mt-5">T-shirts</h3>
+                  <p class="text-base leading-relaxed mt-2">Swag shoivdigoitch literally meditation subway tile tumblr cold-pressed. Gastropub street art beard dreamcatcher neutra, ethical XOXO lumbersexual.</p>
+                  <a target="_self" href="http://webshop.local/shop/" class="text-indigo-500 inline-flex items-center mt-3">
+                     Learn More
+                     <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
+                        <path d="M5 12h14M12 5l7 7-7 7"></path>
+                     </svg>
+                  </a>
+               </div>
+            </div>
+            <div class="p-4 md:w-1/3 sm:mb-0 mb-6">
+               <div class="rounded-lg h-64 overflow-hidden shadow-lg">
+                  <img alt="content" class="object-cover object-center h-full w-full" src="http://webshop.local/wp-content/uploads/2022/10/black_baseball_cap.jpg">
+               </div>
+               <div class="flex flex-col items-center text-center">
+                  <h3 class="text-xl font-semibold title-font text-gray-900 mt-5">Baseball caps</h3>
+                  <p class="text-base leading-relaxed mt-2">Swag shoivdigoitch literally meditation subway tile tumblr cold-pressed. Gastropub street art beard dreamcatcher neutra, ethical XOXO lumbersexual.</p>
+                  <a target="_self" href="http://webshop.local/shop/" class="text-indigo-500 inline-flex items-center mt-3">
+                     Learn More
+                     <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
+                        <path d="M5 12h14M12 5l7 7-7 7"></path>
+                     </svg>
+                  </a>
+               </div>
+            </div>
+         </div>
+      </div>
+   </section>
+   <!-- Content section -->
+</body>
+
+  <?php get_footer(); ?>
+
+
+
+
